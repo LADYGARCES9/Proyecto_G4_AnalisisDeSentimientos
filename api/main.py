7d207c0a11499677dd1237aec84fbfe2365a027d
Dani_P <daniela.pezantez@uees.edu.ec>
2025-08-17 16:59:58 -0500

    Update main.py

--- api/main.py
+++ api/main.py
@@ -181,7 +181,7 @@ def get_result(cid: str):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(
-        "app:app",
+        "main:app",
         host="0.0.0.0",
         port=int(os.getenv("PORT", 8000)),
         log_level="info"
