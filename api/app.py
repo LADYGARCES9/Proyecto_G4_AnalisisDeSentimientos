377db99770e63b8d4ca606eacd1dfabb12b65de5
Dani_P <daniela.pezantez@uees.edu.ec>
2025-08-17 13:34:09 -0500

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,15 +1,20 @@
+import os
+from typing import Optional
 from fastapi import FastAPI
+import uvicorn
+import random
 
 app = FastAPI()
 
 @app.get("/")
-def root():
-    return {"ok": True, "msg": "API up"}
+def home():
+    return {"Hello": "World from FastAPI"}
 
-@app.get("/health")
-def health():
-    return {"status": "ok"}
+# get random number between min(default:0) and max(default:9)
+@app.get("/random/")
+def get_random(min: Optional[int] = 0, max: Optional[int] = 9):
+    rval = random.randint(min, max)
+    return { "value": rval }
 
-@app.get("/test")
-def test():
-    return {"ok": True, "message": "🚀 API funcionando correctamente"}
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
